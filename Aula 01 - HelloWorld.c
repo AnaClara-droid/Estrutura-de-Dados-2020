f55f99ba8d4399a464f48a972036cfe6dedeb1ef
AnaClara-droid <73318733+AnaClara-droid@users.noreply.github.com>
2020-10-28 19:34:29 -0300

    Create Aula 01 - HelloWorld.c

---
+++ Aula 01 - HelloWorld.c
@@ -0,0 +1,12 @@
+/*Esse código possui um método, o int main, que contém um printf que imprime a frase Hello World!*/
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  return 0;
+}
+
+/*
+Hello World!
+*/
